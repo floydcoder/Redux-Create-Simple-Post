33c22b443bb3b113ca76d8de76f9873685812dcf
Marco Stevanella <marcostevanella.canada@gmail.com>
2022-12-29 16:12:15 -0500

    add documentation to post slice

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -2,43 +2,46 @@ import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
 import axios from 'axios';
 
+// API ENDPOINT -> ARRAY OF POSTS
 const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
 
+/**
+ * an Object representing the initial state of the posts
+ * posts: array -> contains all the posts objects
+ * status: string -> is the current status of the posts when syncronized to the API
+ * error: if any error occur
+ */
 const initialState = {
   posts: [],
   status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
 };
 
+// ASYNC API CALLS
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-  try {
-    const response = await axios.get(POST_URL);
-    return [...response.data];
-  } catch (err) {
-    return err.message;
-  }
+  const response = await axios.get(POST_URL);
+  return response.data;
 });
 
 export const addNewPost = createAsyncThunk(
   'posts/addNewPost',
   async (initalPost) => {
-    try {
-      const response = await axios.post(POST_URL, initalPost);
-      return response.data;
-    } catch (err) {
-      return err.message;
-    }
+    const response = await axios.post(POST_URL, initalPost);
+    return response.data;
   }
 );
 
+// POSTS SLICE AND ITS CORRESPONDING REDUCERS
 const postSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
+    // REDUCER -> add a new post
     postAdded: {
       reducer(state, action) {
         state.posts.push(action.payload);
       },
+      // callback
       prepare(title, content, userId) {
         return {
           payload: {
@@ -58,6 +61,7 @@ const postSlice = createSlice({
         };
       },
     },
+    // REDUCER -> add a new reaction
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
       const existingPost = state.posts.find((post) => post.id === postId);
@@ -66,11 +70,14 @@ const postSlice = createSlice({
       }
     },
   },
+  // EXTRA REDUCERS: HANDLE API CALLS BASED ON STATUS
   extraReducers(builder) {
     builder
+      // FETCH POSTS PROMISE IS PENDING
       .addCase(fetchPosts.pending, (state, action) => {
         state.status = 'loading';
       })
+      // FETCH POSTS PROMISE IS FULFILLED
       .addCase(fetchPosts.fulfilled, (state, action) => {
         state.status = 'succeeded';
         // Adding date and reactions
@@ -89,10 +96,13 @@ const postSlice = createSlice({
         // Add any fetched posts to the array
         state.posts = state.posts.concat(loadedPosts);
       })
+      // FETCH POSTS PROMISE IS REJECTED
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       })
+
+      // ADD NEW POST PROMISE IS FULFILLED
       .addCase(addNewPost.fulfilled, (state, action) => {
         action.payload.userId = Number(action.payload.userId);
         action.payload.date = new Date().toISOString();
