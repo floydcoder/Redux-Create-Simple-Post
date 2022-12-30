279a5b19cad23dfdb35dc147a8bf7e7bbb507ab6
Marco Stevanella <marcostevanella.canada@gmail.com>
2022-12-30 17:57:47 -0500

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import PostList from './features/posts/PostList';
 import AddPostForm from './features/posts/AddPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
+import Layout from './componets/Layout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <main className='App'>
-      <AddPostForm />
-      <PostList />
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostList />} />
+
+        <Route path='post'>
+          <Route index element={<AddPostForm />} />
+          <Route path=':postId' element={<SinglePostPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -123,5 +123,11 @@ const postSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+
+// A Selector that retrieves a single post by its ID
+export const selectPostById = (state, postId) => {
+  state.posts.posts.find((post) => post.id === postId);
+};
+
 export const { postAdded, reactionAdded } = postSlice.actions;
 export default postSlice.reducer;
