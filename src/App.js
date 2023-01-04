7eec382b98b9bfd7fdfba61cd91d5113849cd6c4
Marco Stevanella <marcostevanella.canada@gmail.com>
2023-01-03 21:40:21 -0500

    add update functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import PostList from './features/posts/PostList';
 import AddPostForm from './features/posts/AddPostForm';
 import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
 import Layout from './componets/Layout';
 import { Routes, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='post'>
           <Route index element={<AddPostForm />} />
           <Route path=':postId' element={<SinglePostPage />} />
+          <Route path='edit/:postId' element={<EditPostForm />} />
         </Route>
       </Route>
     </Routes>
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -31,6 +31,19 @@ export const addNewPost = createAsyncThunk(
   }
 );
 
+export const updatePost = createAsyncThunk(
+  'posts/updatePost',
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.put(`${POST_URL}/${id}`, initialPost);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+
 // POSTS SLICE AND ITS CORRESPONDING REDUCERS
 const postSlice = createSlice({
   name: 'posts',
@@ -115,6 +128,17 @@ const postSlice = createSlice({
         };
         console.log(action.payload);
         state.posts.push(action.payload);
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log('update could not complete');
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = [...posts, action.payload];
       });
   },
 });
